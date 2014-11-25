d430d2d3f307525593728969d40f057d6b0f7939
Giovanni Blu Mitolo <gioscarab@gmail.com>
2014-11-25 04:11:18 +0100

    Update Agenda.cpp
    Refactoring

--- Agenda/Agenda.cpp
+++ Agenda/Agenda.cpp
@@ -1,5 +1,5 @@
 /*
-  Agenda.cpp - Simple scheduler library.
+  Agenda.cpp - Library for reflectometer sensor.
   Created by Giovanni Blu Mitolo, November 13, 2014.
   Released into the public domain.
 */
@@ -9,49 +9,30 @@
 #include "Agenda.h"
 
 Agenda::Agenda() {
+}
 
-};
-
-Agenda::add(void * task, int timing, byte priority) {
-  for(byte i = 0; i < 256; i++)
-    if(_tasks[i] == nullptr) {
-    
+void Agenda::insert(pointer_to_function task, int timing) {
+  for(byte i = 0; i < 256; i++) 
+    if(_tasks[i] == NULL) {
       _tasks[i] = task;
       _registrations[i] = millis();
       _timings[i] = timing;
-      _priorities[i] = priority;
-      return i;
+    }
+}
 
-    } else if(i == 255) return NULL;
-    
+void Agenda::remove(int id) {
+  _tasks[id] = NULL;
+  _registrations[id] = NULL;
+  _timings[id] = NULL;
+}
 
-};
-
-Agenda::remove(int id) {
-  _tasks[id] = nullptr;
-  _registrations[id] = 0;
-  _timings[id] = 0;
-  _priorities[id] = 0;
-};
-
-Agenda::check() {
+void Agenda::update() {
   unsigned long time = millis();
   
   for(byte i = 0; i < 256; i++) 
-    if(time - registrations[i] > _timings[i])
-      _priorities[i] = 1;
-
-  for(byte i = 0; i < 256; i++) {
-    if(_priorities[i] == 1)
+    if(time - _registrations[i] > _timings[i]) {
       _tasks[i]();
       _registrations[i] = time;
-  }
-
-  for(byte i = 0; i < 256; i++) 
-      _priorities[i] = 0;
-
-};
-
-Agenda::taskList() {
-  return _tasks;
-};
+    }
+  
+}
