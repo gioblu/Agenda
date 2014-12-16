35ae54f32553e3c2a81ddd4f7ae311399712ee2d
Giovanni Blu Mitolo <gioscarab@gmail.com>
2014-12-16 11:22:01 +0100

    Complete refactoring and additions

--- Agenda/Agenda.cpp
+++ Agenda/Agenda.cpp
@@ -1,38 +1,90 @@
-/*
-  Agenda.cpp - Library for reflectometer sensor.
-  Created by Giovanni Blu Mitolo, November 13, 2014.
-  Released into the public domain.
-*/
+/* ___   ___   ___        __   ___
+  |   | | __  |___ |\  | |  \ |   |
+  |___| |   | |    | \ | |   ||___|
+  |   | |___| |___ |  \| |__/ |   | version: 0.1
+
+  Simple Scheduler library for Arduino
+  Giovanni Blu Mitolo 2014 - gioscarab@gmail.com - www.gioblu.com
+  This library has been released on the public domain.
+
+  Agenda agenda;
+  Define an instance of agenda (choose a name, agenda is only an example)
+
+  agenda.insert(function, interval in milliseconds);
+  Add the passed function in the tasks list. Returns a number
+  (the id of the task) or 0 if is impossible to add the task.
+
+  agenda.remove(id);
+  Removes the task from the tasks list that has the same id passed as parameter
+
+  agenda.update();
+  Checks all scheduled tasks and executes which has to be executed. */
 
 #include "WProgram.h"
 #include "WConstants.h"
 #include "Agenda.h"
 
-Agenda::Agenda() {
+struct tasks_struct {
+	void (*execution)(void);
+	unsigned long timing;
+	unsigned long registration;
+  boolean active;
+  boolean execute_once;
+};
+
+tasks_struct _tasks[max_tasks];
+
+Agenda::Agenda(char *time_measure) {
+  if(time_measure == 'microseconds') _time_measure = 'u';
+  if(time_measure == 'milliseconds') _time_measure = 'm';
+  if(time_measure == 'seconds') _time_measure = 's';
 }
 
-void Agenda::insert(pointer_to_function task, int timing) {
-  for(byte i = 0; i < 256; i++) 
+unsigned long Agenda::calculate_time() {
+  if(_time_measure == 'u') return micros();
+  if(_time_measure == 'm') return millis();
+  if(_time_measure == 's') return millis() / 1000;
+}
+
+int Agenda::insert(void (*task)(void), unsigned long timing, boolean once) {
+  for(byte i = 0; i < max_tasks; i++)
     if(_tasks[i] == NULL) {
-      _tasks[i] = task;
-      _registrations[i] = millis();
-      _timings[i] = timing;
+      _tasks[i].active = true;
+      _tasks[i].execution = *task;
+      _tasks[i].registration = calculate_time();
+      _tasks[i].timing = timing;
+      _tasks[i].once = once;
+      return i;
     }
+}
+
+void Agenda::activate(int id) {
+  _tasks[i].active = true;
+}
+
+void Agenda::deactivate(int id) {
+  _tasks[i].active = false;
 }
 
 void Agenda::remove(int id) {
-  _tasks[id] = NULL;
-  _registrations[id] = NULL;
-  _timings[id] = NULL;
+  _tasks[i].active = NULL;
+  _tasks[i].execution = NULL;
+  _tasks[i].registration = NULL;
+  _tasks[i].timing = NULL;
+  _tasks[i].once = NULL;
 }
 
 void Agenda::update() {
-  unsigned long time = millis();
-  
-  for(byte i = 0; i < 256; i++) 
-    if(time - _registrations[i] > _timings[i]) {
-      _tasks[i]();
-      _registrations[i] = time;
+  unsigned long time = calculate_time();
+  for(byte i = 0; i < max_tasks; i++)
+    if((time - tasks[i].registration > _tasks[i].timing) && _tasks[i].active) {
+      _tasks[i].execution();
+      _tasks[i].registration = time;
     }
-  
+}
+
+void Agenda::delay(unsigned long delay_time) {
+  unsigned long time = calculate_time();
+  while(calculate_time() - time < delay_time)
+    this->update();
 }
--- Agenda/Agenda.h
+++ Agenda/Agenda.h
@@ -1,32 +1,53 @@
-/*
-  Agenda.h - Library for reflectometer sensor.
-  Created by Giovanni Blu Mitolo, November 13, 2014.
-  Released into the public domain.
-*/
+/* ___   ___   ___        __   ___
+  |   | | __  |___ |\  | |  \ |   |
+  |___| |   | |    | \ | |   ||___|
+  |   | |___| |___ |  \| |__/ |   | version: 0.1
+
+  Simple Scheduler library for Arduino
+  Giovanni Blu Mitolo 2014 - gioscarab@gmail.com - www.gioblu.com
+  This library has been released on the public domain.
+
+  Agenda agenda;
+  Define an instance of agenda (choose a name, agenda is only an example)
+
+  agenda.insert(function, interval in milliseconds);
+  Add the passed function in the tasks list. Returns a number
+  (the id of the task) or 0 if is impossible to add the task.
+
+  agenda.remove(id);
+  Removes the task from the tasks list that has the same id passed as parameter
+
+  agenda.update();
+  Checks all scheduled tasks and executes which has to be executed. */
 
 #ifndef Agenda_h
-#define Agenda_h
-#include "WProgram.h"
-#include "WConstants.h"
+  #define Agenda_h
+  #include "WConstants.h"
+
+  #if defined(ARDUINO) && (ARDUINO >= 100)
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
 #endif
 
+#define max_tasks 255
+#define agenda_version 01
 
-typedef void (*pointer_to_function)(); // pointer to function with no args and void return
 
 class Agenda {
 
   public:
 
-    Agenda();
-    
-    void insert(pointer_to_function task, int timing);
+    Agenda(char *time_measure);
+
+    int insert(void (*task)(void), unsigned long timing, boolean once = false);
     void remove(int id);
     void update();
+    void delay(unsigned long delay_time);
 
   private:
 
-    pointer_to_function _tasks[255];
-    int  _timings[255];
-    unsigned long _registrations[255];
+    byte _time_measure; 
 
 };
